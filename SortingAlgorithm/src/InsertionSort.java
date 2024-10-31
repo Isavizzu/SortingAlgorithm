97244458a0917aba2390ec77098a381870881f7a
VizzuIsabelle <melovizzu.isabelle@gmail.com>
2024-10-30 22:22:44 -0300

    alterações no insertion

--- SortingAlgorithm/src/InsertionSort.java
+++ SortingAlgorithm/src/InsertionSort.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 
 public class InsertionSort implements SortingAlgorithm {
@@ -9,10 +10,9 @@ public class InsertionSort implements SortingAlgorithm {
             int current = i;
             int j = i-1;
             while (j >= 0) {
-                if (vector.get(current)< vector.get(current-1)) {
-                    Collections.swap(vector, current, current-1);
+                if (vector.get(current)< vector.get(j)) {
+                    Collections.swap(vector, current, j);
                     current--;
-                    j--;
                 }
                 j--;
             }
@@ -21,5 +21,4 @@ public class InsertionSort implements SortingAlgorithm {
         return end - start;
 
     }
-
 }
