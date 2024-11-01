edf2cd297b06edf77e0bee932dd24a876afe3ebe
Maria Fernanda <162508333+Mafezuda@users.noreply.github.com>
2024-11-01 10:46:41 -0300

    Atualizar o BubbleSort.java

--- SortingAlgorithm/src/BubbleSort.java
+++ SortingAlgorithm/src/BubbleSort.java
@@ -5,17 +5,13 @@ public class BubbleSort implements SortingAlgorithm {
 
     public long sort(ArrayList<Integer> vector) {
         long start = System.nanoTime();
-
-        boolean swapped;
+        
         for (int i = 0; i < vector.size() - 1; i++) {
-            swapped = false;
-            for (int j = 0; j < vector.size() - i - 1; j++) {
+            for (int j = 0; j < vector.size() - 1 - i; j++) {
                 if (vector.get(j) > vector.get(j + 1)) {
                     Collections.swap(vector, j, j + 1);
-                    swapped = true;
                 }
             }
-            if (!swapped) break;
         }
 
         long end = System.nanoTime();
