de3edb75d535a37654d5d2e54810dfb2ecf1364d
Maria Fernanda <moreira.mafe@gmail.com>
2024-11-08 12:53:22 -0300

    Revert "."
    This reverts commit a3f823a2fafa61c612283869cb3d1dc82b6082d6.

--- SortingAlgorithm/src/InsertionSort.java
+++ SortingAlgorithm/src/InsertionSort.java
@@ -8,7 +8,7 @@ public class InsertionSort implements SortingAlgorithm {
         for (int i = 1; i < vector.size(); i++) {
             int current = i;
             int j = i-1;
-            for (j >= 0; j > vector.get(j); j++) {
+            while (j >= 0) {
                 if (vector.get(current)< vector.get(j)) {
                     Collections.swap(vector, current, j);
                     current--;
